b182c0d4079c2e72f991d7e1e1f857da3ca6ebb7
Jason Melendez <jason.melendez@gmail.com>
2017-12-17 13:32:09 -0800

    Initial customization from starter project

--- rollup.config.js
+++ rollup.config.js
@@ -30,7 +30,7 @@ export default {
     onwarn: () => { return },
     output: {
         format: 'umd',
-        name: 'ng.angularLibraryStarter',
+        name: 'ng.ngGun',
         globals: globals,
         sourcemap: true,
         exports: 'named'
